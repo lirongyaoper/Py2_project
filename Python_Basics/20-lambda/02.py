92c9e76e4536d0cf78ac17b2dadced908b96255b
lirongyaoper <lirongyaoper@163.com>
2025-06-19 12:43:48 +0800

    update data at 20250619124348

--- Python_Basics/20-lambda/02.py
+++ Python_Basics/20-lambda/02.py
@@ -1,6 +1,16 @@
-def display(name,greeting):
-    result = greeting(name)
-    print(result)
+def get_full_name(first_name,last_name,formatter):
+    return formatter(first_name,last_name)
+
+def first_last(first_name,last_name):
+    return f'{first_name}{last_name}'
+
+def last_first(first_name, last_name):
+    return f'{last_name}{first_name}'
+
+print(get_full_name('jack','li',last_first))
 
 
-display('mary',lambda name:f'Hello {name}')
+
+# lambda 表达式写法
+print(get_full_name('jack','li',lambda first_name,last_name:f'{last_name}{first_name}'))
+print(get_full_name('jack','li',lambda first_name,last_name:f'{first_name}{last_name}'))
